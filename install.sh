3115eb06577f9eb7155d69d42037149fe7d36fce
Rodney Richard <ruddyrod82@gmail.com>
2024-09-19 04:01:03 -0400

    Added install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+mkdir .ssh
+for privkey in $(ls *.crypt | awk -F '.' '{print $1}'); do
+  cat $privkey.crypt | openssl enc -d -des3 -base64 -pbkdf2 | tee .ssh/$privkey
+  chmod 0400 .ssh/$privkey
+  cp $privkey.pub .ssh
+done
+
+mv .ssh ~
